666f5679d3108e0c91b35870953fefc1c6f29149
Nikita Romanyuk <kelvium@yahoo.com>
2022-11-02 22:22:54 +0300

    don't align keymap

--- keyboards/annepro2/keymaps/Kelvium/keymap.c
+++ keyboards/annepro2/keymaps/Kelvium/keymap.c
@@ -25,17 +25,17 @@ enum anne_pro_layers {
 // clang-format off
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[BASE] = LAYOUT_60_ansi(
-		KC_ESC,           KC_1,    KC_2,    KC_3, KC_4, KC_5, KC_6,   KC_7, KC_8, KC_9,    KC_0,             KC_MINS,          KC_EQL,        KC_BSPC,
-		KC_TAB,           KC_Q,    KC_W,    KC_E, KC_R, KC_T, KC_Y,   KC_U, KC_I, KC_O,    KC_P,             KC_LBRC,          KC_RBRC,       KC_BSLS,
-		KC_CAPS,          KC_A,    KC_S,    KC_D, KC_F, KC_G, KC_H,   KC_J, KC_K, KC_L,    KC_SCLN,          KC_QUOT,          KC_ENT,
-		KC_LSFT,                   KC_Z,    KC_X, KC_C, KC_V, KC_B,   KC_N, KC_M, KC_COMM, KC_DOT,           KC_SLSH,          KC_RSFT,
-		KC_LCTL,          KC_LGUI, KC_LALT,                   KC_SPC,             KC_RALT, MO(FN1),          KC_RGUI,          KC_RCTL),
+		KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
+		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
+		KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
+		KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+		KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, MO(FN1), KC_RGUI, KC_RCTL),
 
 	[FN1] = LAYOUT_60_ansi(
-		KC_TILD,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,         KC_F6,         KC_F7,         KC_F8,         KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-		_______, _______, _______,  KC_UP,   _______, KC_AP2_BT1,    KC_AP2_BT2,    KC_AP2_BT3,    KC_AP2_BT4,    _______, KC_PSCR, KC_HOME, KC_END,  _______,
-		_______, _______, KC_LEFT,  KC_DOWN, KC_RGHT, KC_AP_RGB_MOD, KC_AP_RGB_TOG, KC_AP_RGB_VAD, KC_AP_RGB_VAI, _______, KC_PGUP, KC_PGDN, _______,
-		_______,          KC_VOLU,  KC_VOLD, KC_MUTE, _______,       _______,       _______,       _______,       _______, KC_INS,  KC_DEL,  _______,
-		_______, _______, _______,                                   _______,                                     _______, _______, _______, _______),
+		KC_TILD, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL,
+		_______, _______, _______, KC_UP, _______, KC_AP2_BT1, KC_AP2_BT2, KC_AP2_BT3, KC_AP2_BT4, _______, KC_PSCR, KC_HOME, KC_END, _______,
+		_______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_AP_RGB_MOD, KC_AP_RGB_TOG, KC_AP_RGB_VAD, KC_AP_RGB_VAI, _______, KC_PGUP, KC_PGDN, _______,
+		_______, KC_VOLU, KC_VOLD, KC_MUTE, _______, _______, _______, _______, _______, KC_INS, KC_DEL, _______,
+		_______, _______, _______, _______, _______, _______, _______, _______),
 };
 // clang-format on
